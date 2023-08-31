2bdc8dd61745fe74fae45dcfbff3e480642315c2
sarkarniloy102 <sarkarniloy102@gmail.com>
2023-09-01 00:48:16 +0600

    Show Details Modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const LoadPhone = async (PhoneName, isshow) => {
+const LoadPhone = async (PhoneName = 'a', isshow) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${PhoneName}`);
     const data = await res.json();
     const phones = data.data;
@@ -21,7 +21,7 @@ const DisplayPhones = (phones, isshow) => {
     if (!isshow)
         phones = phones.slice(0, 9);
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // create a div 
         const PhoneCard = document.createElement('div');
         // create class for div
@@ -36,7 +36,7 @@ const DisplayPhones = (phones, isshow) => {
         <p>There are many variations of passages of available, but the majority have suffered</p>
         <p>$999</p>
         <div class="card-actions">
-            <button class="btn btn-primary">Show Details</button>
+            <button onclick="HandleShowDetailsModal('${phone.slug}')" class="btn btn-primary">Show Details</button>
         </div>
     </div>`
         // adding div in container
@@ -64,7 +64,37 @@ const ToggleLoadSpin = (isload) => {
 }
 // handle show button
 const HandleShowBtn = () => {
-    console.log("hello");
+    // console.log("hello");
     SearchHandle(true);
+}
+// Handle show details modal
+const HandleShowDetailsModal = async (id) => {
+    // console.log(id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    console.log(phone);
+    ShowDetailsModal(phone);
+}
+// show the modal
+const ShowDetailsModal = (phone) => {
+    show_details.showModal();
+    const ModalContainer = document.getElementById('modal_container');
+    ModalContainer.innerHTML = `
+    <div class="bg-[#0d6efd0d] p-5">
+    <img class="mx-auto " src="${phone.image}" alt=""/>
+    </div>
+    
+    <h3 class="font-bold mb-3">${phone.name}</h3>
+    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+    <p> <span class="font-bold mb-3">Storage:</span>  ${phone.mainFeatures.storage}</p>
+    <p> <span class="font-bold mb-3">Display Size:</span>  ${phone.mainFeatures.displaySize}</p>
+    <p> <span class="font-bold mb-3">Chipset:</span>  ${phone.mainFeatures.chipSet}</p>
+    <p> <span class="font-bold mb-3">Memory:</span>  ${phone.mainFeatures.memory}</p>
+    <p> <span class="font-bold mb-3">Slug:</span>  ${phone.slug}</p>
+    <p> <span class="font-bold mb-3">Release Date:</span>  ${phone.releaseDate}</p>
+    <p> <span class="font-bold mb-3">Brand:</span>  ${phone.brand}</p>
+    <p> <span class="font-bold mb-3">GPS:</span>  ${phone.others.GPS}</p>
+    `
 }
 LoadPhone();
